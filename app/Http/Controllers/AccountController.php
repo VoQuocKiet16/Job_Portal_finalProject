0d46f25102485b668291092e57acdfe01393fc1b
VoQuocKiet16 <vok89265@gmail.com>
2024-02-25 18:11:20 +0700

    Update Profile

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -11,75 +11,120 @@ use Illuminate\Support\Facades\Validator;
 class AccountController extends Controller
 {
     // This method will show registration page
-    public function registration() {
-        return view ('front.account.registration');
+    public function registration()
+    {
+        return view('front.account.registration');
     }
- 
+
     //This method will save a user
-    public function processRegistration(Request $request){
-        $validator = Validator::make($request->all(),[
+    public function processRegistration(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
             'name' => 'required',
             'email' => 'required|email|unique:users,email',
             'password' => 'required|min:8|same:confirm_password',
             'confirm_password' => 'required',
         ]);
-        
-        if($validator->passes()){
-        
+
+        if ($validator->passes()) {
+
             $user = new User();
-            $user -> name = $request->name;
-            $user -> email = $request->email;
-            $user -> password = Hash::make($request->password);
-            $user -> save();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
 
             session()->flash(
-               'success',
-               'You have registered successfully.'
+                'success',
+                'You have registered successfully.'
             );
 
             return response()->json([
                 'status' => true,
                 'errors' => []
-            ]);  
-
-        }else{
+            ]);
+        } else {
             return response()->json([
                 'status' => false,
-                'errors' => $validator ->errors()
+                'errors' => $validator->errors()
             ]);
         }
     }
 
     // This method will show login page
-    public function login() {
-        return view ('front.account.login');
+    public function login()
+    {
+        return view('front.account.login');
     }
 
-    public function authenticate(Request $request){
-        $validator = Validator::make($request->all(),[
+    public function authenticate(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
             'email' => 'required',
             'password' => 'required'
         ]);
 
-        if($validator->passes()){
-            if(Auth::attempt(['email'=>$request->email,'password'=>$request->password])){
+        if ($validator->passes()) {
+            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
                 return redirect()->route('account.profile');
-            }else{
-                return redirect()->route('account.login')->with('error','Email or Password is incorrect');
+            } else {
+                return redirect()->route('account.login')->with('error', 'Email or Password is incorrect');
             }
-        }else{
+        } else {
             return redirect()->route('account.login')
-            ->withErrors($validator)
-            ->withInput($request->only('email'));
+                ->withErrors($validator)
+                ->withInput($request->only('email'));
         }
-
     }
 
-    public function profile() {
-        return view('front.account.profile');
+    // This method will show profile page
+    public function profile()
+    {
+        $id = Auth::user()->id;
+
+        $user = User::where('id', $id)->first();
+
+        return view('front.account.profile', [
+            'user' => $user
+        ]);
     }
 
-    public function logout() {
+    // This method will update profile
+    public function updateProfile(Request $request)
+    {
+
+        $id = Auth::user()->id;
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:5|max:20',
+            'email' => 'required|email|unique:users,email,' . $id . ',id'
+        ]);
+
+        if ($validator->passes()) {
+            $user = User::find($id);
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->mobile = $request->mobile;
+            $user->designation = $request->designation;
+            $user->save();
+
+            session()->flash('success', 'Profile update successfully.');
+
+            return response()->json([
+                'status' => true,
+                'errors' => []
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+    }
+
+    // This method will logout account
+    public function logout()
+    {
         Auth::logout();
         return redirect()->route('account.login');
     }
