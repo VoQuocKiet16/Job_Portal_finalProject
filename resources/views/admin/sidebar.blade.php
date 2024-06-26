39a70342783b82d4683ce6d3529c926f89d50958
VoQuocKiet16 <vok89265@gmail.com>
2024-06-26 23:41:29 +0700

    Update in Admin

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -2,10 +2,10 @@
     <div class="card-body p-0">
         <ul class="list-group list-group-flush ">
             <li class="list-group-item d-flex justify-content-between p-3">
-                <a href="">Users</a>
+                <a href="{{route('admin.users')}}">Users</a>
             </li>
             <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                <a href="">Jobs</a>
+                <a href="{{route('admin.jobs')}}">Jobs</a>
             </li>
             <li class="list-group-item d-flex justify-content-between align-items-center p-3">
                 <a href="">Job Applications</a>
